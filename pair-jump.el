c2e6e9806bdf436846d67e9029d64798ae1d4816
AbyChan <crystal.tyan@gmail.com>
2015-12-24 11:27:54 +0800

    implemented

--- pair-jump.el
+++ pair-jump.el
@@ -1,12 +1,22 @@
 
 (defun pair-jump-or-tab ()
   (interactive)
-  (message "try pair")
   (indent-for-tab-command)
-  )
+  (let ((after-char (string (char-after)))
+        (before-char (string (char-before))))
+    (cond ((or (equal after-char "(")
+               (equal after-char "{")
+               (equal after-char "["))
+           (forward-sexp))
+          ((or (equal after-char ")")
+               (equal after-char "}")
+               (equal after-char "]"))
+           (backward-sexp)))))
+                                        ;(thing-at-point 'char)
 
 (global-set-key [tab] 'pair-jump-or-tab)
 (global-set-key (kbd "TAB") 'pair-jump-or-tab)
 
-
+                                        ;backward-sexp
+                                        ;forward-sexp
 (provide 'pair-jump)
